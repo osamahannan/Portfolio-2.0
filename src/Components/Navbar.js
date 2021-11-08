17670a8e47eae395337ce44fd276fee67f2a0113
osamahannan <83069516+osamahannan@users.noreply.github.com>
2021-11-08 12:32:13 +0530

    updated project section

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import logo from "../Assets/logo.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className ="navbar">
+        <div className="navbar">
             <div className="logo">
-                <img src={logo} alt="Osama hannan" />
+                <Link to="/" className="osamahannan">Osama Hannan</Link>
             </div>
 
             <ul>
-                <li><Link to ="/"> HOME </Link></li>
-                <li><Link to ="/projects"> PROJECTS </Link></li>
-                <li><Link to =""> CONTACT </Link></li>
+                <li><Link to="/"> HOME </Link></li>
+                <li><Link to="/projects"> PROJECTS </Link></li>
+                <li><Link to=""> CONTACT </Link></li>
             </ul>
         </div>
     )
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,8 +2,53 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div className ="projects">
-            <h1>This is Projects page</h1>
+        <div className="projects">
+            <h1> Projects </h1>
+            <div className="project-card2">
+                <div className="card2">
+                    <h3>CryptoSwitch</h3>
+                    <small>
+                        <time>10th October, 2021</time>
+                    </small>
+                    <p>
+                        Created an app using React.Js . It is an application that displays all the trending cryptocurrencies. It allows you to add, update and delete any coin in your wallet. It also shows graphical representation of any cryptocurrency. You can see it working here:
+                    </p>
+
+                    <div className="button-container">
+                        <button className="btn view"><a href="https://cryptoswitch.netlify.app/" className="anchor" target="_blank" rel="noreferrer">View Live</a></button>
+                        <button className="btn2"><a href="https://github.com/osamahannan/CryptoSwitch" className="anchor" target="_blank" rel="noreferrer">View Source</a></button>
+                    </div>
+                </div>
+                <div className="card2">
+                    <h3>MERN Register</h3>
+                    <small>
+                        <time>15th September, 2021</time>
+                    </small>
+                    <p>
+                        Created an app using MERN stack. This app allows you to register youself and create a profile of your own. All your data is being stored at the backend in a database using MongoDB.You can also contact and message for any query.
+                    </p>
+
+                    <div className="button-container">
+                        <button className="btn view"><a href="https://mern-osama.herokuapp.com" className="anchor" target="_blank" rel="noreferrer">View Live</a></button>
+                        <button className="btn2"><a href="https://github.com/osamahannan/MERN_Register" className="anchor" target="_blank" rel="noreferrer">View Source</a></button>
+                    </div>
+                </div>
+                <div className="card2">
+                    <h3>Tic Tac Toe</h3>
+                    <small>
+                        <time>25th July, 2021</time>
+                    </small>
+                    <p>
+                        Created an app in Javascript. Tic-tac-toe is a simple, two-player game that, if played optimally by both players, will always result in a tie. The game is also called noughts and crosses or Xs and Os. A player wins a game if three marks of the same kind is connected in a line.
+                    </p>
+
+                    <div className="button-container">
+                        <button className="btn view"><a href="https://tic-tac-toe-carnage.netlify.app" className="anchor" target="_blank" rel="noreferrer">View Live</a></button>
+                        <button className="btn2"><a href="https://github.com/osamahannan/Tic_Tac_Toe-Game" className="anchor" target="_blank" rel="noreferrer">View Source</a></button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
